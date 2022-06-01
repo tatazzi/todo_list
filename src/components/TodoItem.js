e487c30c9d5f6766f534fa75500f15c9fd9f53a5
tatazzi <otaviol.gon@gmail.com>
2022-05-31 22:12:07 -0300

    testing native checkbox

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,8 +1,61 @@
-import React, {useState} from 'react';
-import {View, TouchableOpacity, StyleSheet, Text} from 'react-native';
+import React, {useEffect, useState, useRef} from 'react';
+import {
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  Text,
+  Image,
+  Animated,
+} from 'react-native';
 import CheckBox from '@react-native-community/checkbox';
 import {useDispatch} from 'react-redux';
 import {removeTodo} from '../reducers/todoSlice';
+
+// const CheckBox = ({value, onValueChange}) => {
+//   const fadeAnim = useRef(new Animated.Value(0)).current;
+
+//   useEffect(() => {
+//     Animated.timing(fadeAnim, {
+//       toValue: value ? 0 : 1,
+//     duration:200,
+//       useNativeDriver: false,
+//     }).start();
+//   }, [value]);
+
+//   const opacity = fadeAnim.interpolate({
+//     inputRange: [0, 1],
+//     outputRange: [0, 1],
+//   });
+
+//   return (
+//     <TouchableOpacity
+//       activeOpacity={0.8}
+//       onPress={() => onValueChange(!value)}
+//       style={checkboxStyle.wrapper}>
+//       <Animated.Image
+//         source={require('../check.png')}
+//         resizeMode="contain"
+//         style={{...checkboxStyle.image, opacity}}
+//       />
+//     </TouchableOpacity>
+//   );
+// };
+
+// const checkboxStyle = StyleSheet.create({
+//   wrapper: {
+//     width: 36,
+//     height: 36,
+//     borderWidth: 1,
+//     borderRadius: 5,
+//     borderColor: 'black',
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   },
+//   image: {
+//     width: 20,
+//     height: 16.5,
+//   },
+// });
 
 const TodoItem = ({title, id}) => {
   const [toggleCheckBox, setToggleCheckBox] = useState(false);
